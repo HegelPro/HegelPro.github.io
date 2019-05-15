60faa8c9f5a0fbf0d5885143ac41c43f4bf950c9
HegelPro <34520941+HegelPro@users.noreply.github.com>
2019-05-15 14:08:54 +0400

    add react-2048

---
+++ react-2048/precache-manifest.b7fa46d0dc6394b5657e0ac58576572f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "412e169b9afe7d5d861a",
+    "url": "/static/js/main.2fc840b4.chunk.js"
+  },
+  {
+    "revision": "54d7e0fb9518f8d54713",
+    "url": "/static/js/2.b0d83681.chunk.js"
+  },
+  {
+    "revision": "412e169b9afe7d5d861a",
+    "url": "/static/css/main.d1c47897.chunk.css"
+  },
+  {
+    "revision": "f7555cb8ff69ce27afa923209544944b",
+    "url": "/index.html"
+  }
+];
